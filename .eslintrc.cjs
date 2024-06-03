4943b6c2b865e5210947aad0324831becc7a7408
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-03 09:32:02 +0200

    Styling guide config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,24 +2,24 @@ module.exports = {
   root: true,
   env: { browser: true, es2021: true },
   extends: [
-    'airbnb',
-    'airbnb-base',
-    'prettier',
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "airbnb",
+    "airbnb/hooks",
+    "prettier",
+    "eslint:recommended",
+    "eslint:all",
+    "plugin:prettier/recommended"
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['prettier', 'react-refresh'],
-  rules: {
-    'prettier/prettier': 'error',
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-}
+  settings: { react: { version: "detect" } },
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
