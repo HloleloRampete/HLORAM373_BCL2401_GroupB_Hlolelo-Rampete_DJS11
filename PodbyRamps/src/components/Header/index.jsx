e7806c62d0cf5c28d46c134c35b9694731eccdfe
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-13 15:51:22 +0200

    "Updated Header component to add active class to links based on current path, and added active class styles to CSS"

--- PodbyRamps/src/components/Header/index.jsx
+++ PodbyRamps/src/components/Header/index.jsx
@@ -1,15 +1,29 @@
-import "./styles.css"
-import { Link } from "react-router-dom"
+import "./styles.css";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Header() {
-    return (
+  const location = useLocation();
+  const currentPath = location.pathname;
+  return (
     <div className="navbar">
-        <div className="gradient"></div>
-        <div className="links">
-            <Link to="/">Signup</Link>
-            <Link to="/podcasts">Podcasts</Link>
-            <Link to="/start-a-podcats">Start A Podcats</Link>
-            <Link to="/profile">Profile</Link>
-        </div>
-        </div>)
+      <div className="gradient"></div>
+      <div className="links">
+        <Link to="/" className={currentPath == "/" ? "active" : ""}>
+          Signup
+        </Link>
+        <Link to="/podcasts" className={currentPath == "/podcasts" ? "active" : ""}>
+          Podcasts
+        </Link>
+        <Link
+          to="/create-a-podcast"
+          className={currentPath == "/create-a-podcast" ? "active" : ""}
+        >
+          Start A Podcats
+        </Link>
+        <Link to="/profile" className={currentPath == "/profile" ? "active" : ""}>
+          Profile
+        </Link>
+      </div>
+    </div>
+  );
 }
