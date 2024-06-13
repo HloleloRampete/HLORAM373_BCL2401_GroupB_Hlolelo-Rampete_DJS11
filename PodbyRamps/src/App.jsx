808f95ce122608b8e4969b8a626e38556253c6f0
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-13 10:01:12 +0200

    "Added react-router-dom to App.jsx and created a route for SignUpPage; updated SignUpPage to include Header component"

--- PodbyRamps/src/App.jsx
+++ PodbyRamps/src/App.jsx
@@ -1,12 +1,18 @@
+import { Route, Router, Routes } from "react-router-dom"
 import Header from "./components/Header/index"
 import './App.css'
+import SignUpPage from "./pages/SignUp"
 
 export default function App() {
   
 
   return (
-    <div>
-      <Header />
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<SignUpPage />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
--- PodbyRamps/src/pages/SignUp.jsx
+++ PodbyRamps/src/pages/SignUp.jsx
@@ -1,5 +1,9 @@
+import Header from "../components/Header";
+
 export default function SignUpPage() {
-    return (
-        <div>SignUpPage</div>
-    )
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
