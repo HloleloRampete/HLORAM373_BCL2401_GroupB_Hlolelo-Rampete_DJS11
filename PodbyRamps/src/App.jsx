b90c90f9f7df390b67629067b44ba56da2315164
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-13 15:15:53 +0200

    "Remove App.css, reorder react-router-dom imports in App.jsx, and modify Header component styles"

--- PodbyRamps/src/App.jsx
+++ PodbyRamps/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import SignUpPage from "./pages/SignUp";
 
