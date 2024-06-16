ca8d38d184f071c9396b60c5a72c3b128fcacbfe
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-16 10:13:44 +0200

    "Added useState hook and InputComponent to SignUpPage, replaced 'Hello World' header with a full name input field."

--- PodbyRamps/src/pages/SignUp.jsx
+++ PodbyRamps/src/pages/SignUp.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import Header from "../components/Header";
+import InputComponent from "../components/Input";
 
 export default function SignUpPage() {
+  const [fullName, setFullName] = useState("");
   return (
     <div>
       <Header />
-      <h1>Hello World</h1>
+
+      <div>
+        <InputComponent
+          state={fullName}
+          setState={setFullName}
+          placeholder="Full Name"
+          type="text"
+          required={true}
+        />
+      </div>
     </div>
   );
 }
