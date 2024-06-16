cf73e81560a71367812ff300115ca92e351dd472
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-16 12:04:00 +0200

    "Removed individual input fields and button, replaced with SignupForm component, and added toggle functionality for Signup/Login"

--- PodbyRamps/src/pages/SignUp.jsx
+++ PodbyRamps/src/pages/SignUp.jsx
@@ -1,28 +1,27 @@
 import { useState } from "react";
 import Header from "../components/CommonComponents/Header";
-import InputComponent from "../components/CommonComponents/Input";
-import Button from "../components/CommonComponents/Button";
 import SignupForm from "../components/SignupComponents/SignupForm";
 
 export default function SignUpPage() {
-  const [fullName, setFullName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [flag, setFlag] = useState(false)
+  const [flag, setFlag] = useState(false);
 
-  const handleSignup = () => {
-    // Implement your signup logic here
-  }
 
   return (
     <div>
       <Header />
 
       <div className="input-wrapper">
-        <h1>Signup</h1>
-        <SignupForm />
-        <p>Click here if you already have an Account. Login.</p>
+        {!flag ? <h1>Signup</h1> : <h1>Login</h1>}
+        {!flag ? <SignupForm /> : <></>}
+        {!flag ? (
+          <p onClick={() => setFlag(!flag)}>
+            Already have an Account? Click here to Login.
+          </p>
+        ) : (
+          <p onClick={() => setFlag(!flag)}>
+            Do not have an account? Click here to Signup.
+          </p>
+        )}
       </div>
     </div>
   );
