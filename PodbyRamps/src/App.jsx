2a48989fdc21e73c3536b7d02babcac5347a2af0
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-17 21:18:14 +0200

    Added ToastContainer from react-toastify to App component.

--- PodbyRamps/src/App.jsx
+++ PodbyRamps/src/App.jsx
@@ -2,10 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import SignUpPage from "./pages/SignUpPage";
 import Profile from "./pages/Profile";
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 export default function App() {
   return (
     <div className="App">
+      <ToastContainer />
       <Router>
         <Routes>
           <Route path="/" element={<SignUpPage />} />
