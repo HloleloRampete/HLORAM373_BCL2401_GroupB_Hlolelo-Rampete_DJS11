31fc6dc82da34c2894a76abb7e5880f81641d9cb
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-17 10:57:00 +0200

    "Renamed Signup.jsx to SignupPage.jsx

--- PodbyRamps/src/App.jsx
+++ PodbyRamps/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import SignUpPage from "./pages/SignUp";
+import SignUpPage from "./pages/SignUpPage";
 
 export default function App() {
   return (
