82e28f0f10ffea00bafe44eeef1f1b23c77c0e53
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-17 19:59:29 +0200

    "Added react-toastify dependency and implemented login functionality with Firebase authentication and Redux state management in LoginForm component"

--- PodbyRamps/src/components/SignupComponents/LoginForm/index.jsx
+++ PodbyRamps/src/components/SignupComponents/LoginForm/index.jsx
@@ -1,14 +1,46 @@
-import { useState } from "react"
-import InputComponent from "../../CommonComponents/Input/index"
-import Button from "../../CommonComponents/Button"
+import { useState } from "react";
+import InputComponent from "../../CommonComponents/Input/index";
+import Button from "../../CommonComponents/Button";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../../../firebase";
+import { doc, getDoc } from "firebase/firestore";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setUser } from "../../../slices/userSlice";
+import { toast } from "react-toastify";
 
 export default function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
+  const handleLogin = async () => {
+    // Implement your slogin logic here
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
 
-  const handleLogin = () => {
-    // Implement your signup logic here
+      const userDoc = await getDoc(doc(db, "users", user.uid));
+      const userData = userDoc.data();
+
+      dispatch(
+        setUser({
+          name: userData.name,
+          email: user.email,
+          uid: user.uid,
+          profilePic: userData.profilePic,
+        })
+      );
+      toast.success("User Login Successful!");
+      navigate("/profile");
+    } catch (error) {
+      toast.error("Invalid Credentials!");
+    }
   };
 
   return (
