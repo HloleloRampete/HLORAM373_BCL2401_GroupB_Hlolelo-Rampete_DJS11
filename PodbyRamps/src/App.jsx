322a6960e4efd5f60fae21e6b26936fd427774f9
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-17 19:12:57 +0200

    "Added Profile page route to App component"

--- PodbyRamps/src/App.jsx
+++ PodbyRamps/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import SignUpPage from "./pages/SignUpPage";
+import Profile from "./pages/Profile";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Router>
         <Routes>
           <Route path="/" element={<SignUpPage />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </Router>
     </div>
