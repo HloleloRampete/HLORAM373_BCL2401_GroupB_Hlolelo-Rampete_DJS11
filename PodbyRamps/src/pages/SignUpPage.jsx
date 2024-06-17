82c7aa28bf153e7a656e63563a1e3b90477699e2
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-17 11:09:33 +0200

    "Added LoginForm component to SignUpPage and updated conditional rendering logic"

--- PodbyRamps/src/pages/SignUpPage.jsx
+++ PodbyRamps/src/pages/SignUpPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "../components/CommonComponents/Header";
 import SignupForm from "../components/SignupComponents/SignupForm";
+import LoginForm from "../components/SignupComponents/LoginForm";
 
 export default function SignUpPage() {
   const [flag, setFlag] = useState(false);
@@ -12,13 +13,13 @@ export default function SignUpPage() {
 
       <div className="input-wrapper">
         {!flag ? <h1>Signup</h1> : <h1>Login</h1>}
-        {!flag ? <SignupForm /> : <></>}
+        {!flag ? <SignupForm /> : <LoginForm/>}
         {!flag ? (
-          <p onClick={() => setFlag(!flag)}>
+          <p style={{cursor: "pointer"}} onClick={() => setFlag(!flag)}>
             Already have an Account? Click here to Login.
           </p>
         ) : (
-          <p onClick={() => setFlag(!flag)}>
+          <p style={{cursor: "pointer"}} onClick={() => setFlag(!flag)}>
             Do not have an account? Click here to Signup.
           </p>
         )}
