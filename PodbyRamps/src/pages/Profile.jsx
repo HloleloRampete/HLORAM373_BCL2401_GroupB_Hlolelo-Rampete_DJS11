180fe3e2fb8f0d316d65ad66f1bbdc219842c471
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-17 20:48:22 +0200

    "Added import for react-redux and Header component, and modified Profile component to use useSelector to retrieve user data and display it."

--- PodbyRamps/src/pages/Profile.jsx
+++ PodbyRamps/src/pages/Profile.jsx
@@ -1,5 +1,15 @@
-export default function Profile() {
-    return (<h1>Profile</h1>
+import { useSelector } from "react-redux"
+import Header from "../components/CommonComponents/Header"
 
+export default function Profile() {
+    const user = useSelector((state) => state.user.user)
+    console.log("My User", user)
+    return (
+        <div>
+            <Header />
+            <h1>{user.name}</h1>
+            <h1>{user.email}</h1>
+            <h1>{user.uid}</h1>
+        </div>
     )
 }
