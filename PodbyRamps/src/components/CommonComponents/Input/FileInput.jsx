2f8c7d7fc0431495c1a777da706e95b818fd960b
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-18 15:00:58 +0200

    "Updated FileInput component to add state for file selection, new label styling, and onChange event handler."

--- PodbyRamps/src/components/CommonComponents/Input/FileInput.jsx
+++ PodbyRamps/src/components/CommonComponents/Input/FileInput.jsx
@@ -1,8 +1,30 @@
-export default function FileInput({ accept, id }) {
+import { useState } from "react";
+import "./styles.css"
+
+export default function FileInput({ accept, id, fileHandleFnc, text }) {
+  const [fileSelected, setFileSelected] = useState("");
+
+  const onChange = (e) => {
+    console.log(e.target.files);
+    setFileSelected(e.target.files[0].name);
+    fileHandleFnc(e.target.files[0]);
+  };
+
   return (
     <>
-      <label htmlFor={id}>Click Me</label>
-      <input type="file" accept={accept} id={id} style={{ display: "none" }} />
+      <label
+        htmlFor={id}
+        className={`custom-input ${!fileSelected ? "label-input" : "active"}`}
+      >
+        {fileSelected ? `The File ${fileSelected} was Selected` : text}
+      </label>
+      <input
+        type="file"
+        accept={accept}
+        id={id}
+        style={{ display: "none" }}
+        onChange={onChange}
+      />
     </>
   );
 }
