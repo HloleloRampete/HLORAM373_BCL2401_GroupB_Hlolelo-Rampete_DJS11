d45d060c4dd005297449def09124eca4ceb4dba2
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-18 13:20:10 +0200

    "Updated App.jsx to use PrivateRoutes component and refactored auth state handling; added PrivateRoutes.jsx component to handle private routes"

--- PodbyRamps/src/components/CommonComponents/PrivateRoutes.jsx
+++ PodbyRamps/src/components/CommonComponents/PrivateRoutes.jsx
@@ -1,6 +1,6 @@
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Outlet, Navigate } from "react-router-dom";
-import { auth } from "../firebase";
+import { auth } from "../../firebase";
 
 export default function PrivateRoutes() {
   const [user, loading, error] = useAuthState(auth);
