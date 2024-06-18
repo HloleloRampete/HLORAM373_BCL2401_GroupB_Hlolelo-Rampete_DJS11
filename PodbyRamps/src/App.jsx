49404d81e875472069603bcbfe6dceea2d7a44ad
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-18 10:27:51 +0200

    "Added auth state change listener and user data to set up an authentication state change listener that fetches user data from Firestore when the user is authenticated"

--- PodbyRamps/src/App.jsx
+++ PodbyRamps/src/App.jsx
@@ -4,8 +4,47 @@ import SignUpPage from "./pages/SignUpPage";
 import Profile from "./pages/Profile";
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth, db } from "./firebase";
+import { doc, onSnapshot } from "firebase/firestore";
+import { setUser } from "./slices/userSlice";
+import { useDispatch } from "react-redux";
 
 export default function App() {
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    const unsubscribeAuth = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const unsubscribeSnapshot = onSnapshot(
+          doc(db, "users", user.uid), 
+          (userDoc) => {
+            if (userDoc.exists()) {
+              const userData = userDoc.data();
+              dispatch(setUser({
+                name: userData.name,
+                email: userData.email,
+                uid: user.uid,
+              })
+            );
+          }
+        },
+        (error) => {
+          console.error("Error fetching user data:", error);
+        }
+      );
+
+      return () => {
+        unsubscribeSnapshot();
+      };
+    }
+  });
+
+  return () => {
+    unsubscribeAuth();
+  };
+}, []);
   return (
     <div className="App">
       <ToastContainer />
