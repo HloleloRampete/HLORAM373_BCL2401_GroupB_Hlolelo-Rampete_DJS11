71196389b8b30700faafe8d8c944eec5d0ba6574
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-18 13:54:24 +0200

    Added a div with a heading to the CreateAPodcast component.

--- PodbyRamps/src/pages/CreateAPodcast.jsx
+++ PodbyRamps/src/pages/CreateAPodcast.jsx
@@ -4,6 +4,9 @@ export default function CreateAPodcast() {
   return (
     <div>
       <Header />
+      <div className="input-wrapper">
+        <h1>Create A Podcast</h1>
+      </div>
     </div>
   );
 }
