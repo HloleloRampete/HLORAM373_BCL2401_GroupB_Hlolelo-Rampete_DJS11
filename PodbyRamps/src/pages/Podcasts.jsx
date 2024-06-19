bd3593327e4c2d64e500dfab703f8c487ab6d0db
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-19 13:03:35 +0200

    "Imported Header in Podcasts.jsx and added it in the return of PodcastsPage with an h1"

---
+++ PodbyRamps/src/pages/Podcasts.jsx
@@ -0,0 +1,9 @@
+import Header from "../components/CommonComponents/Header/index"
+export default function PodcastsPage() {
+    return (
+        <div>
+            <Header />
+            <h1>Discover Podcasts</h1>
+        </div>
+    )
+}
