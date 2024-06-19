98c129ae5c3c0b10f70e1a8b04f40b718168d11b
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-19 14:01:50 +0200

    "Updated PodcastCard component to include Link to podcast details, display image, and title"

--- PodbyRamps/src/components/Podcasts/PodcastCard/index.jsx
+++ PodbyRamps/src/components/Podcasts/PodcastCard/index.jsx
@@ -1,4 +1,12 @@
+import { Link } from "react-router-dom";
 import "./styles.css";
-export default function PodcastsCard({ title, displayImage }) {
-  return <div className="podcast-card">PodcastCard</div>;
+export default function PodcastsCard({ id, title, displayImage }) {
+  return (
+    <Link to={`/podcast/${id}`}>
+      <div className="podcast-card">
+        <img src={displayImage} alt={title} className="podcast-image" />
+        <p>{title}</p>
+      </div>
+    </Link>
+  );
 }
