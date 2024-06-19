7234c9ed0cf6804380bf8bbeb152cefbeaa002df
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-19 15:10:01 +0200

    "Refactored PodcastsPage component: renamed PodcastsCard to PodcastCard, added InputComponent for search, and updated filtering logic"

--- PodbyRamps/src/pages/Podcasts.jsx
+++ PodbyRamps/src/pages/Podcasts.jsx
@@ -3,59 +3,70 @@ import Header from "../components/CommonComponents/Header/index";
 import { collection, onSnapshot, query } from "firebase/firestore";
 import { db } from "../firebase";
 import { useDispatch, useSelector } from "react-redux";
-import PodcastsCard from "../components/Podcasts/PodcastCard";
+import PodcastCard from "../components/Podcasts/PodcastCard";
 import { setPodcasts } from "../slices/podcastSlice"
+import InputComponent from "../components/CommonComponents/Input";
 
 export default function PodcastsPage() {
-  const dispatch = useDispatch();
-  const podcasts = useSelector((state) => state.podcasts.podcasts);
-    const [search, setSearch] = useState("")
-
-  useEffect(() => {
-    const unsubscribe = onSnapshot(
-      query(collection(db, "podcasts")),
-      (querySnapshot) => {
-        const podcastsData = [];
-        querySnapshot.forEach((doc) => {
-          podcastsData.push({ id: doc.id, ...doc.data() });
-        });
-        dispatch(setPodcasts(podcastsData));
-      },
-      (error) => {
-        console.error("Error fetching podcats:", error);
-      }
+    const dispatch = useDispatch();
+    const podcasts = useSelector((state) => state.podcasts.podcasts);
+    const [search, setSearch] = useState("");
+  
+    useEffect(() => {
+      const unsubscribe = onSnapshot(
+        query(collection(db, "podcasts")),
+        (querySnapshot) => {
+          const podcastsData = [];
+          querySnapshot.forEach((doc) => {
+            podcastsData.push({ id: doc.id, ...doc.data() });
+          });
+          dispatch(setPodcasts(podcastsData));
+        },
+        (error) => {
+          console.error("Error fetching podcasts:", error);
+        }
+      );
+  
+      return () => {
+        unsubscribe();
+      };
+    }, [dispatch]);
+  
+    console.log(podcasts);
+  
+    let filteredPodcasts = podcasts.filter((item) =>
+      item.title.trim().toLowerCase().includes(search.trim().toLowerCase())
     );
-
-    return () => {
-      unsubscribe();
-    };
-  }, [dispatch]);
-
-  console.log(podcasts)
-  let filteredPodcasts = podcasts.filter((item) => item.title.toLowerCase().includes(search.toLowerCase))
-
-  return (
-    <div>
-      <Header />
-      <div className="input-wrapper" style={{ marginTop: "2rem" }}>
-        <h1>Discover Podcasts</h1>
-        {podcasts.length > 0 ? (
-          <div className="podcasts-layout">
-            {podcasts.map((item) => {
-              return (
-                <PodcastsCard
-                  key={item.id}
-                  id={item.id}
-                  title={item.title}
-                  displayImage={item.displayImage}
-                />
-              );
-            })}
-          </div>
-        ) : (
-          <p>No Podcasts Found</p>
-        )}
+  
+    return (
+      <div>
+        <Header />
+        <div className="input-wrapper" style={{ marginTop: "2rem" }}>
+          <h1>Discover Podcasts</h1>
+          <InputComponent
+            state={search}
+            setState={setSearch}
+            placeholder="Search By Title"
+            type="text"
+          />
+  
+          {filteredPodcasts.length > 0 ? (
+            <div className="podcasts-flex" style={{ marginTop: "1.5rem" }}>
+              {filteredPodcasts.map((item) => {
+                return (
+                  <PodcastCard
+                    key={item.id}
+                    id={item.id}
+                    title={item.title}
+                    displayImage={item.displayImage}
+                  />
+                );
+              })}
+            </div>
+          ) : (
+            <p>{search ? "Podcast Not Found" : "No Podcasts On The Platform"}</p>
+          )}
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
