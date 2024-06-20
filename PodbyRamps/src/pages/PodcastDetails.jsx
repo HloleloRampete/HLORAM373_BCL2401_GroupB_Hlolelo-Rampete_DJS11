a620a4865d2206ef33a85c668be6ad0875502fea
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-20 15:26:13 +0200

    "Removed AudioPlayer import in PodcastDetailsPage and updated package-lock.json and package.json with new dependencies"

--- PodbyRamps/src/pages/PodcastDetails.jsx
+++ PodbyRamps/src/pages/PodcastDetails.jsx
@@ -5,7 +5,7 @@ import { toast } from "react-toastify";
 import Button from "../components/CommonComponents/Button";
 import { auth } from "../firebase";
 import EpisodeDetails from "../components/Podcasts/EpisodeDetails";
-import AudioPlayer from "../components/Podcasts/AudioPlayer";
+// import AudioPlayer from "../components/Podcasts/AudioPlayer";
 
 export default function PodcastDetailsPage() {
   const { id } = useParams();
