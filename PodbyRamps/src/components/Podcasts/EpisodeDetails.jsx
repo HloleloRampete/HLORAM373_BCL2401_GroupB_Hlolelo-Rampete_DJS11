c426b1893eadef69dfded8f8243ceda84e217bbd
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-20 15:07:23 +0200

    "Updated index.css with new styles and refactored PodcastDetailsPage.jsx to fetch podcast data and render details"

---
+++ PodbyRamps/src/components/Podcasts/EpisodeDetails.jsx
@@ -0,0 +1,22 @@
+import Button from "../CommonComponents/Button";
+
+export default function EpisodeDetails({ index, title, description, audioFile, onClick }) {
+    return (
+    
+      <div style={{ width: "100%" }}>
+        <h1 style={{ textAlign: "left", marginBottom: 0 }}>
+          {index}. {title}
+        </h1>
+        <p style={{ marginLeft: "1.5rem" }} className="podcast-description ">
+          {description}
+        </p>
+        <Button
+          text={"Play"}
+          onClick={() => onClick(audioFile)}
+          width={"100px"}
+        />
+      </div>
+    );
+  }
+
+  // still to add audio
